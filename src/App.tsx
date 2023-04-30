6b4063dc46af06b4c93b76f631116abc1fab34bf
MarVic <55230624+MarVicFr@users.noreply.github.com>
2023-04-30 15:55:44 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
+      <section
+        className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full 
+                          lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded text-zinc-700 "
+      >
+        Hello world
+      </section>
+    </main>
+  )
+}
+
+export default App
